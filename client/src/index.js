2d3f3a0c35de2812255322307318f1f02776ddd6
joranwa <joranwa@student.matnat.uio.no>
2022-01-31 13:18:08 +0200

    Code should soon start working again... maybe...

--- client/src/index.js
+++ client/src/index.js
@@ -9,16 +9,21 @@ class FormRoot extends React.Component {
         this.state = {
             imp_name: "",
             numProducts: 1,
-            data: {}
+            data: {},
+            products: [0]
         };
         this.inputChanged = this.inputChanged.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.addProduct = this.addProduct.bind(this);
+        this.getData = this.getData.bind(this);
+        this.renderProductList = this.renderProductList.bind(this);
+        this.renderSingleProduct = this.renderSingleProduct.bind(this);
     }
 
     componentDidMount() {
         this.setState({
             importer: <ImporterInfo  name={this.inputChanged}/>,
-            products: <ProductList name={this.inputChanged}/>,
+            //products: <ProductList name={this.inputChanged}/>,
         })
     }
 
@@ -37,11 +42,180 @@ class FormRoot extends React.Component {
                     <p>{this.state.imp_name}</p>
                 </div>
                 {this.state.importer}
-                {this.state.products}
+                {this.renderProductList()}
             </form>
         </div>
         );
     }
+
+    renderProductList() {
+        var i = 0;
+            return (
+                <div className='products'>
+                    <div className='productsList'>
+                    <h3>Produkter</h3>
+                    <hr></hr>
+                    {
+                        this.state.products.map((e) => {
+                        return <div key={e}>
+                        <h3><b>{(++i)}. vin:</b></h3>
+                        {console.log("AAAAA")}
+                        {this.renderSingleProduct(i)}                     
+                        </div>;
+                    })}
+                    </div>
+                    <div className='newProductBtn'>
+                        <button onClick={this.addProduct}>
+                            Legg til et produkt
+                        </button>
+                    </div>
+                    <div className='submitBtn'>
+                        <button type="submit" name="send">
+                            Send inn
+                        </button>
+                    </div>
+                </div>
+            );
+    }
+        
+    renderSingleProduct(num) {
+        return (
+            <div id={'div'+num}>
+                <p><i> bare varenummer-feltet er obligatorisk </i></p>
+                <label htmlFor="varenummerVin"> Varenummer Vinmonopolet: 
+                    <span className="req"> * </span> 
+                </label>
+                <br></br>
+                <input name="varenummerVin" type="text" required id={"varenummerVin"+num} value={this.state.productNumber} 
+                onChange={(e) => { this.setState({[e.target.id]: e.target.value}); console.log(e.target.id)}} ></input>
+                <br></br>
+                <div id="popupBtn">
+                <button className="searchBtn" type="button" 
+                                onClick={() => {this.getData(num)}} name="søk etter vin"> 
+                                 Søk etter vin 
+                            </button>,
+                </div>
+                <div className="vin" id="divVinInfo" style={{display: this.state.display}}>
+                <label htmlFor="produsentVin"> Produsent: <span className="req">  </span> </label>
+                <br></br>
+                <input name="produsentVin" type="text" id={"produsentVin"+num} value={this.state.produsent}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="hjemmesideVin"> Produsents hjemmeside: </label> 
+                <br></br>
+                <input name="hjemmesideVin" type="text" id={"'hjemmesideVin"+num} value={this.state.hjemmeside}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="omradeVin"> Område og land: <span className="req">  </span> </label> 
+                <br></br>
+                <input name="omradeVin" type="text" id={"'omradeVin"+num} value={this.state.omrade}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="drueVin"> Drue- og drueblanding:
+                    <span className="req">  </span> 
+                </label> 
+                <br></br>
+                <input name="drueVin" type="text" id={"'drueVin"+num} value={this.state.druer}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="navnVin"> Vinens navn: <span className="req">  </span> </label> 
+                <br></br>
+                <input name="navnVin" type="text" id={"'navnVin"+num} value={this.state.navn}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="argangVin"> Årgang: <span className="req">  </span> </label> 
+                <br></br>
+                <input name="argang" type="text" id={"'argangVin"+num} value={this.state.argang}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="prisVin"> Pris Vinmonopolet: </label> 
+                <br></br>
+                <input name="prisVin" type="text" id={"'prisVin"+num} value={this.state.pris}
+                onChange={e => this.setState({[e.target.id]: e.target.value})}>
+                </input>
+                <br></br>
+                <label htmlFor="linkVin"> Link til Vinmonopolets side for vinen: </label> 
+                <br></br>
+                <input name="linkVin" type="text" id={"'linkVin"+num} value={this.state.produktside}
+                onChange={e => this.setState({produktside: e.target.value})}>
+                </input>
+                <br></br>
+            </div>                <hr></hr>
+            </div>
+        );   
+    }
+
+   /**
+     * Adds a new Product object.
+     * It is added to the tail of the relevant array
+     * & is given the property 'num' with the length of said array.
+     */
+    addProduct() {
+        console.log("Legger til nytt produkt...");
+        this.setState({
+            numProducts: (this.state.numProducts+1),
+            products: [...this.state.products, this.state.numProducts],
+
+        });
+        console.log(this.state.numProducts);
+    }
+
+           /**
+     * called when the "search"-button is pressed.
+     * If necessary, re-renders the object with {display:block}.
+     * @param isVisible: is True if neither the "search" or "manual"
+     *      buttons has been pressed yet
+     */
+    getData(refNum) {
+        let prodNum = (this.state["varenummerVin"+refNum]);
+        console.log(prodNum);
+        this.searchForProductInfo(prodNum, refNum);
+    }
+
+    /**
+     * Initiates an asynchronous api request to "/api/vp".
+     * "/api/vp" is used to fetch product information from Vinmonopolet.
+     * 
+     * Once finished, either alert user of an error or use the
+     * data fetched to populate the relevant input fields.
+     */
+    searchForProductInfo(prodNum, refNum) {
+        //const product = this.state.productNumber;
+        console.log("Søker opp produkt " + prodNum);
+        fetch("/api/vp?product="+prodNum)
+        .then((res) =>  res.json())
+        .then(data => { 
+            console.log(data);
+            if (data.error != "false" || data.data == "")
+            alert(`Fant ikke produktinformasjon.\n${data.error}`);
+            else {
+                const vin = data.data;
+                var druer = "";
+                for (let i = 0; i < (vin.ingredients.grapes).length; i++)
+                {
+                    druer += `${vin.ingredients.grapes[i].grapeDesc} ${vin.ingredients.grapes[i].grapePct}%, `
+                }
+                druer = druer.slice(0, druer.length-2); //End trailing chars
+                this.setState({
+                    ["productIdVin"+refNum]: vin.basic.productId,
+                    ["produsentVin"+refNum]: vin.logistics.manufacturerName,
+                    ["omradeVin"+refNum]: `${vin.origins.origin.country}, ${vin.origins.origin.region}`,
+                    ["druerVin"+refNum]: druer,
+                    ["navnVin"+refNum]: vin.basic.productLongName,
+                    ["argangVin"+refNum]: vin.basic.vintage,
+                    ["prisVin"+refNum]: vin.prices[0].salesPrice,
+                    ["produktsideVin"+refNum]: `www.vinmonopolet.no/p/${vin.basic.productId}`
+                });
+            }
+        });
+    }
+
 
     /**
      * Generic function that updates stat values.
@@ -126,9 +300,7 @@ class ProductList extends React.Component {
                     return <div key={e.props.num}>
                     <h3><b>{(++i)}. vin:</b></h3>
                     {console.log("AAAAA")}
-                    {this.renderSingleWine(i)}
-                    {console.log(this.state.varenummerVin1)}
-                     
+                    {this.renderSingleProduct(i)}                     
                     </div>;
                 })}
                 </div>
@@ -147,8 +319,37 @@ class ProductList extends React.Component {
     }
 
 
+renderProductList() {
+    var i = 0;
+        return (
+            <div className='products'>
+                <div className='productsList'>
+                <h3>Produkter</h3>
+                <hr></hr>
+                {
+                    this.state.products.map((e) => {
+                    return <div key={e.props.num}>
+                    <h3><b>{(++i)}. vin:</b></h3>
+                    {console.log("AAAAA")}
+                    {this.renderSingleProduct(i)}                     
+                    </div>;
+                })}
+                </div>
+                <div className='newProductBtn'>
+                    <button onClick={this.addProduct}>
+                        Legg til et produkt
+                    </button>
+                </div>
+                <div className='submitBtn'>
+                    <button type="submit" name="send">
+                        Send inn
+                    </button>
+                </div>
+            </div>
+        );
+}
     
-renderSingleWine(num) {
+renderSingleProduct(num) {
     return (
         <div id={'div'+num}>
             <p><i> bare varenummer-feltet er obligatorisk </i></p>
